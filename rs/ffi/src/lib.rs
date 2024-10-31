bb800bb20dd1c430d68d260a2b96f7d3b37cfa32
Dravia Vemal <contact@draviavemal.com>
2024-10-31 08:05:37 +0000

    cmake improvising

--- rs/ffi/src/lib.rs
+++ rs/ffi/src/lib.rs
@@ -1,10 +1,10 @@
 use openxmloffice_spreadsheet::Excel;
 
 #[no_mangle]
-pub extern "C" fn create_excel(file_name: Option<String>) -> *mut Excel {
+pub extern "C" fn create_excel(file_name: Option<String>) {
     if let Some(file_name) = file_name {
-        return Excel::new(Some(file_name));
+        // return Excel::new(Some(file_name));
     } else {
-        return Excel::new(None);
+        // return Excel::new(None);
     }
 }
