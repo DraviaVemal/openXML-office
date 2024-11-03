bab15c43c51f48fc99b8f0072dfe4a87307023c2
Dravia Vemal <contact@draviavemal.com>
2024-11-03 08:49:55 +0000

    proto buf schema design update

--- rs/spreadsheet/src/lib.rs
+++ rs/spreadsheet/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod enums;
 pub mod implements;
+pub mod schema;
 pub mod structs;
 pub mod tests;
 pub mod utils;
 
+pub use schema::ExcelProperties::*;
 pub use structs::excel::*;
--- rs/spreadsheet/src/tests/test_excel.rs
+++ rs/spreadsheet/src/tests/test_excel.rs
@@ -1,6 +1,6 @@
 #[test]
 fn blank_excel() {
-    let file = crate::Excel::new(None);
+    let file = crate::Excel::new(None, None);
     file.add_sheet(&"Test".to_string());
     file.save_as(&"this.xlsx".to_string());
     assert_eq!(true, true);
