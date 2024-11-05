12176a14d78cc0b8f80266fb569f8a60f8e1a576
Dravia Vemal <contact@draviavemal.com>
2024-11-05 15:16:37 +0000

    Decode encode file

--- rs/xml/src/utils/file_handling.rs
+++ rs/xml/src/utils/file_handling.rs
@@ -1,11 +1,19 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
+use flate2::bufread::GzDecoder;
 use flate2::write::GzEncoder;
 use flate2::Compression;
-use std::io::Write;
+use std::io::{Read, Write};
 
-pub fn compress_content(data: &[u8]) -> Result<Vec<u8>> {
+pub fn compress_content(uncompressed_data: &[u8]) -> Result<Vec<u8>, Error> {
     let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
-    encoder.write_all(data)?;
+    encoder.write_all(uncompressed_data)?;
     let compressed_data = encoder.finish()?;
     Ok(compressed_data)
+}
+
+pub fn decompress_content(compressed_data: &[u8]) -> Result<Vec<u8>, Error> {
+    let mut decoder = GzDecoder::new(compressed_data);
+    let mut decompressed_data = Vec::new();
+    decoder.read_to_end(&mut decompressed_data)?;
+    Ok(decompressed_data)
 }
