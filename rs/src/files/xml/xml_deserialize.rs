d605dfe9f8279004f5f217225741573d6c0a8d14
Dravia Vemal <contact@draviavemal.com>
2024-11-20 09:29:11 +0000

    Starting deserialization

--- rs/src/files/xml/xml_deserialize.rs
+++ rs/src/files/xml/xml_deserialize.rs
@@ -2,7 +2,7 @@ use crate::files::XmlElement;
 pub struct XmlDeSerializer {}
 
 impl XmlDeSerializer {
-    pub fn xml_tree_to_xml_vec(_xml_element: XmlElement) -> Vec<u8> {
+    pub fn xml_tree_to_xml_vec(_xml_element: &XmlElement) -> Vec<u8> {
         return Vec::new();
     }
 }
