d564b9bb6ba4affa6ded49816a0c938d9738e639
Dravia Vemal <contact@draviavemal.com>
2024-12-05 10:57:15 +0000

    Spreadsheet Style Service

--- rs/src/spreadsheet/spreadsheet_2007/models/style.rs
+++ rs/src/spreadsheet/spreadsheet_2007/models/style.rs
@@ -1,64 +1,350 @@
-pub enum HorizontalAlignmentValues {
-    NONE,
-    LEFT,
-    CENTER,
-    RIGHT,
-    JUSTIFY,
+use crate::global_2007::traits::Enum;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum SchemeValues {
+    None,
+    Minor,
+    Major,
 }
-pub enum VerticalAlignmentValues {
-    NONE,
-    TOP,
-    MIDDLE,
-    BOTTOM,
+
+impl Enum<SchemeValues> for SchemeValues {
+    fn get_string(input_enum: SchemeValues) -> String {
+        match input_enum {
+            SchemeValues::Major => "major".to_string(),
+            SchemeValues::Minor => "minor".to_string(),
+            SchemeValues::None => "none".to_string(),
+        }
+    }
+    fn get_enum(input_string: &str) -> SchemeValues {
+        match input_string {
+            "major" => SchemeValues::Major,
+            "minor" => SchemeValues::Minor,
+            _ => SchemeValues::None,
+        }
+    }
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum PatternTypeValues {
+    None,
+    Gray125,
+    Solid,
+}
+
+impl Enum<PatternTypeValues> for PatternTypeValues {
+    fn get_string(input_enum: PatternTypeValues) -> String {
+        match input_enum {
+            PatternTypeValues::Solid => "solid".to_string(),
+            PatternTypeValues::Gray125 => "gray125".to_string(),
+            PatternTypeValues::None => "none".to_string(),
+        }
+    }
+
+    fn get_enum(input_string: &str) -> PatternTypeValues {
+        match input_string {
+            "solid" => PatternTypeValues::Solid,
+            "gray125" => PatternTypeValues::Gray125,
+            _ => PatternTypeValues::None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum BorderStyleValues {
+    None,
+    Thin,
+    Thick,
+    Dotted,
+    Double,
+    Dashed,
+    DashDot,
+    DashDotDot,
+    Medium,
+    MediumDashed,
+    MediumDashDot,
+    MediumDashDotDot,
+    SlantDashDot,
+    Hair,
+}
+impl Enum<BorderStyleValues> for BorderStyleValues {
+    fn get_string(input_enum: BorderStyleValues) -> String {
+        match input_enum {
+            BorderStyleValues::DashDot => "dashDot".to_string(),
+            BorderStyleValues::DashDotDot => "dashDotDot".to_string(),
+            BorderStyleValues::Dashed => "dashed".to_string(),
+            BorderStyleValues::Dotted => "dotted".to_string(),
+            BorderStyleValues::Double => "double".to_string(),
+            BorderStyleValues::Hair => "hair".to_string(),
+            BorderStyleValues::Medium => "medium".to_string(),
+            BorderStyleValues::MediumDashDot => "mediumDashDot".to_string(),
+            BorderStyleValues::MediumDashDotDot => "mediumDashDotDot".to_string(),
+            BorderStyleValues::MediumDashed => "mediumDashed".to_string(),
+            BorderStyleValues::SlantDashDot => "slantDashDot".to_string(),
+            BorderStyleValues::Thick => "thick".to_string(),
+            BorderStyleValues::Thin => "thin".to_string(),
+            BorderStyleValues::None => "none".to_string(),
+        }
+    }
+
+    fn get_enum(input_string: &str) -> BorderStyleValues {
+        match input_string {
+            "dashDot" => BorderStyleValues::DashDot,
+            "dashDotDot" => BorderStyleValues::DashDotDot,
+            "dashed" => BorderStyleValues::Dashed,
+            "dotted" => BorderStyleValues::Dotted,
+            "double" => BorderStyleValues::Double,
+            "hair" => BorderStyleValues::Hair,
+            "medium" => BorderStyleValues::Medium,
+            "mediumDashDot" => BorderStyleValues::MediumDashDot,
+            "mediumDashDotDot" => BorderStyleValues::MediumDashDotDot,
+            "mediumDashed" => BorderStyleValues::MediumDashed,
+            "slantDashDot" => BorderStyleValues::SlantDashDot,
+            "thick" => BorderStyleValues::Thick,
+            "thin" => BorderStyleValues::Thin,
+            _ => BorderStyleValues::None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub enum ColorSettingTypeValues {
-    INDEXED,
-    THEME,
-    RGB,
-}
-pub enum StyleValues {
-    NONE,
-    THIN,
-    THICK,
-    DOTTED,
-    DOUBLE,
-    DASHED,
-    DASH_DOT,
-    DASH_DOT_DOT,
-    MEDIUM,
-    MEDIUM_DASHED,
-    MEDIUM_DASH_DOT,
-    MEDIUM_DASH_DOT_DOT,
-    SLANT_DASH_DOT,
-    HAIR,
+    Indexed,
+    Theme,
+    Rgb,
 }
 
+impl Enum<ColorSettingTypeValues> for ColorSettingTypeValues {
+    fn get_string(input_enum: ColorSettingTypeValues) -> String {
+        match input_enum {
+            ColorSettingTypeValues::Theme => "theme".to_string(),
+            ColorSettingTypeValues::Rgb => "rgb".to_string(),
+            ColorSettingTypeValues::Indexed => "indexed".to_string(),
+        }
+    }
+    fn get_enum(input_string: &str) -> ColorSettingTypeValues {
+        match input_string {
+            "theme" => ColorSettingTypeValues::Theme,
+            "rgb" => ColorSettingTypeValues::Rgb,
+            _ => ColorSettingTypeValues::Indexed,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ColorSetting {
-    color_setting_type_values: ColorSettingTypeValues,
-    value: String,
+    pub color_setting_type: ColorSettingTypeValues,
+    pub value: String,
 }
 
+impl Default for ColorSetting {
+    fn default() -> Self {
+        Self {
+            color_setting_type: ColorSettingTypeValues::Theme,
+            value: "1".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct BorderSetting {
-    border_color: ColorSetting,
-    style: StyleValues,
+    pub border_color: Option<ColorSetting>,
+    pub style: BorderStyleValues,
 }
 
+impl Default for BorderSetting {
+    fn default() -> Self {
+        Self {
+            border_color: None,
+            style: BorderStyleValues::None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct BorderStyle {
+    pub id: u32,
+    pub bottom: BorderSetting,
+    pub left: BorderSetting,
+    pub right: BorderSetting,
+    pub top: BorderSetting,
+}
+
+impl Default for BorderStyle {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            bottom: BorderSetting::default(),
+            left: BorderSetting::default(),
+            right: BorderSetting::default(),
+            top: BorderSetting::default(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum HorizontalAlignmentValues {
+    None,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum VerticalAlignmentValues {
+    None,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
 pub struct CellStyleSetting {
-    background_color: String,
-    border_bottom: BorderSetting,
-    border_left: BorderSetting,
-    border_right: BorderSetting,
-    border_top: BorderSetting,
-    font_family: String,
-    font_size: usize,
-    foreground_color: String,
-    is_bold: bool,
-    is_double_underline: bool,
-    is_italic: bool,
-    is_underline: bool,
-    is_wrap_text: bool,
-    number_format: String,
-    text_color: ColorSetting,
-    horizontal_alignment: HorizontalAlignmentValues,
-    vertical_alignment: VerticalAlignmentValues,
+    pub background_color: Option<String>,
+    pub border_bottom: BorderSetting,
+    pub border_left: BorderSetting,
+    pub border_right: BorderSetting,
+    pub border_top: BorderSetting,
+    pub font_family: String,
+    pub font_size: u32,
+    pub foreground_color: Option<String>,
+    pub horizontal_alignment: HorizontalAlignmentValues,
+    pub is_bold: bool,
+    pub is_double_underline: bool,
+    pub is_italic: bool,
+    pub is_underline: bool,
+    pub is_wrap_text: bool,
+    pub number_format: String,
+    pub text_color: ColorSetting,
+    pub vertical_alignment: VerticalAlignmentValues,
+}
+
+impl Default for CellStyleSetting {
+    fn default() -> Self {
+        Self {
+            background_color: None,
+            border_bottom: BorderSetting::default(),
+            border_left: BorderSetting::default(),
+            border_right: BorderSetting::default(),
+            border_top: BorderSetting::default(),
+            font_family: "Calibri".to_string(),
+            font_size: 11,
+            foreground_color: None,
+            horizontal_alignment: HorizontalAlignmentValues::None,
+            is_bold: false,
+            is_double_underline: false,
+            is_italic: false,
+            is_underline: false,
+            is_wrap_text: false,
+            number_format: "General".to_string(),
+            text_color: ColorSetting {
+                color_setting_type: ColorSettingTypeValues::Rgb,
+                value: "000000".to_string(),
+            },
+            vertical_alignment: VerticalAlignmentValues::None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CellXfs {
+    pub id: u32,
+    pub format_id: u32,
+    pub number_format_id: u32,
+    pub font_id: u32,
+    pub fill_id: u32,
+    pub border_id: u32,
+    pub apply_font: bool,
+    pub apply_alignment: bool,
+    pub apply_fill: bool,
+    pub apply_border: bool,
+    pub apply_number_format: bool,
+    pub apply_protection: bool,
+    pub is_wrap_text: bool,
+    pub horizontal_alignment: HorizontalAlignmentValues,
+    pub vertical_alignment: VerticalAlignmentValues,
+}
+
+impl Default for CellXfs {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            format_id: 0,
+            number_format_id: 0,
+            font_id: 0,
+            fill_id: 0,
+            border_id: 0,
+            apply_font: false,
+            apply_alignment: false,
+            apply_fill: false,
+            apply_border: false,
+            apply_number_format: false,
+            apply_protection: false,
+            is_wrap_text: false,
+            horizontal_alignment: HorizontalAlignmentValues::None,
+            vertical_alignment: VerticalAlignmentValues::None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FillStyle {
+    pub id: u32,
+    pub pattern_type: PatternTypeValues,
+    pub background_color: Option<ColorSetting>,
+    pub foreground_color: Option<ColorSetting>,
+}
+
+impl Default for FillStyle {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            background_color: None,
+            foreground_color: None,
+            pattern_type: PatternTypeValues::None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FontStyle {
+    pub id: u32,
+    pub name: String,
+    pub size: u32,
+    pub color: ColorSetting,
+    pub family: i32,
+    pub font_scheme: SchemeValues,
+    pub is_bold: bool,
+    pub is_double_underline: bool,
+    pub is_italic: bool,
+    pub is_underline: bool,
+}
+
+impl Default for FontStyle {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            color: ColorSetting {
+                value: "1".to_string(),
+                ..Default::default()
+            },
+            family: 2,
+            font_scheme: SchemeValues::None,
+            is_bold: false,
+            is_double_underline: false,
+            is_italic: false,
+            is_underline: false,
+            name: "Calibri".to_string(),
+            size: 11,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NumberFormats {
+    pub id: u32,
+    pub format_code: String,
+}
+
+impl Default for NumberFormats {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            format_code: "General".to_string(),
+        }
+    }
 }
--- rs/src/spreadsheet/spreadsheet_2007/services/style.sql
+++ rs/src/spreadsheet/spreadsheet_2007/services/style.sql
@@ -1,166 +1,198 @@
 -- query : create_number_format_table# 
 CREATE TABLE
-    number_formats
-(
-    id          INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
-    format_id   INTEGER NOT NULL UNIQUE,           -- Number Format code
-    format_code TEXT    NOT NULL                   -- Number Format code
-);
+    number_formats (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
+        format_id INTEGER NOT NULL UNIQUE, -- Number Format code
+        format_code TEXT NOT NULL -- Number Format code
+    );
 
 -- query : create_font_style_table# 
 CREATE TABLE
-    font_style
-(
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
-    font_name           TEXT NOT NULL,                     -- Style Name
-    color_rgb           TEXT,                              -- Color RGB
-    color_theme         INTEGER,                           -- Color Theme
-    family_id           INTEGER,                           -- Font Family Id
-    font_size           INTEGER,                           -- Font Size
-    font_scheme         TEXT,                              -- Font Schema
-    is_bold             INTEGER,                           -- Is Bold BOOL
-    is_italic           INTEGER,                           -- Is Italic BOOL
-    is_underline        INTEGER,                           -- Is Underline BOOL
-    is_double_underline INTEGER                            -- Is Double Underline BOOL
-);
+    font_style (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
+        font_name TEXT NOT NULL, -- Style Name
+        color_type TEXT, -- Color Setting
+        color_value TEXT, -- Color Setting
+        family_id INTEGER, -- Font Family Id
+        font_size INTEGER, -- Font Size
+        font_scheme TEXT, -- Font Schema
+        is_bold INTEGER, -- Is Bold BOOL
+        is_italic INTEGER, -- Is Italic BOOL
+        is_underline INTEGER, -- Is Underline BOOL
+        is_double_underline INTEGER -- Is Double Underline BOOL
+    );
 
 -- query : create_fill_style_table# 
 CREATE TABLE
-    fill_style
-(
-    id                       INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
-    background_color_setting TEXT,                              -- Background Color Setting JSON
-    foreground_color_setting TEXT,                              -- Foreground Color Setting JSON
-    pattern_type             TEXT                               -- Pattern Type JSON
-);
+    fill_style (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
+        background_color_setting TEXT, -- Background Color Setting JSON
+        foreground_color_setting TEXT, -- Foreground Color Setting JSON
+        pattern_type TEXT -- Pattern Type Enum
+    );
 
 -- query : create_border_style_table# 
 CREATE TABLE
-    border_style
-(
-    id            INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
-    left_border   TEXT,                              -- Left border setting JSON
-    top_border    TEXT,                              -- Top border setting JSON
-    right_border  TEXT,                              -- Right border setting JSON
-    bottom_border TEXT                               -- Bottom border setting JSON
-);
+    border_style (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
+        left_border TEXT, -- Left border setting JSON
+        top_border TEXT, -- Top border setting JSON
+        right_border TEXT, -- Right border setting JSON
+        bottom_border TEXT, -- Bottom border setting JSON
+        diagonal_border TEXT -- Bottom border setting JSON
+    );
 
 -- query : create_cell_style_table# 
 CREATE TABLE
-    cell_xfs
-(
-    id                     INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
-    format_id              INTEGER,                           -- Format Id
-    number_format_id       INTEGER,                           -- Number Format Id
-    font_id                INTEGER,                           -- Font Id
-    fill_id                INTEGER,                           -- Fill Id
-    border_id              INTEGER,                           -- Border Id
-    is_apply_font          INTEGER,                           -- Apply font flag BOOL
-    is_apply_alignment     INTEGER,                           -- Apply alignment flag BOOL
-    is_apply_fill          INTEGER,                           -- Apply fill flag BOOL
-    is_apply_border        INTEGER,                           -- Apply border flag BOOL
-    is_apply_number_format INTEGER,                           -- Apply number format flag BOOL
-    is_apply_protection    INTEGER,                           -- Apply protection flag BOOL
-    is_wrap_text           INTEGER,                           -- Wrap cell text flag BOOL
-    horizontal_alignment   TEXT,                              -- Horizontal alignment setting JSON
-    vertical_alignment     TEXT                               -- Vertical alignment setting JSON
-);
+    cell_xfs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, -- Unique ID for each file
+        format_id INTEGER, -- Format Id
+        number_format_id INTEGER, -- Number Format Id
+        font_id INTEGER, -- Font Id
+        fill_id INTEGER, -- Fill Id
+        border_id INTEGER, -- Border Id
+        is_apply_font INTEGER, -- Apply font flag BOOL
+        is_apply_alignment INTEGER, -- Apply alignment flag BOOL
+        is_apply_fill INTEGER, -- Apply fill flag BOOL
+        is_apply_border INTEGER, -- Apply border flag BOOL
+        is_apply_number_format INTEGER, -- Apply number format flag BOOL
+        is_apply_protection INTEGER, -- Apply protection flag BOOL
+        is_wrap_text INTEGER, -- Wrap cell text flag BOOL
+        horizontal_alignment TEXT, -- Horizontal alignment setting JSON
+        vertical_alignment TEXT -- Vertical alignment setting JSON
+    );
 
 -- query : insert_number_format_table# 
-INSERT INTO number_formats (format_code, format_id)
-VALUES (?, ?);
+INSERT INTO
+    number_formats (format_code, format_id)
+VALUES
+    (?, ?);
 
 -- query : insert_font_style_table# 
-INSERT INTO font_style (font_name,
-                        color_rgb,
-                        color_theme,
-                        family_id,
-                        font_size,
-                        font_scheme,
-                        is_bold,
-                        is_italic,
-                        is_underline,
-                        is_double_underline)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO
+    font_style (
+        font_name,
+        color_type,
+        color_value,
+        family_id,
+        font_size,
+        font_scheme,
+        is_bold,
+        is_italic,
+        is_underline,
+        is_double_underline
+    )
+VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 
 -- query : insert_fill_style_table# 
-INSERT INTO fill_style (background_color_setting,
-                        foreground_color_setting,
-                        pattern_type)
-VALUES (?, ?, ?);
+INSERT INTO
+    fill_style (
+        background_color_setting,
+        foreground_color_setting,
+        pattern_type
+    )
+VALUES
+    (?, ?, ?);
 
 -- query : insert_border_style_table# 
-INSERT INTO border_style (left_border,
-                          top_border,
-                          right_border,
-                          bottom_border)
-VALUES (?, ?, ?, ?);
+INSERT INTO
+    border_style (
+        left_border,
+        top_border,
+        right_border,
+        bottom_border,
+        diagonal_border
+    )
+VALUES
+    (?, ?, ?, ?, ?);
 
 -- query : insert_cell_style_table# 
-INSERT INTO cell_xfs (format_id,
-                      number_format_id,
-                      font_id,
-                      fill_id,
-                      border_id,
-                      is_apply_font,
-                      is_apply_alignment,
-                      is_apply_fill,
-                      is_apply_border,
-                      is_apply_number_format,
-                      is_apply_protection,
-                      is_wrap_text,
-                      horizontal_alignment,
-                      vertical_alignment)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO
+    cell_xfs (
+        format_id,
+        number_format_id,
+        font_id,
+        fill_id,
+        border_id,
+        is_apply_font,
+        is_apply_alignment,
+        is_apply_fill,
+        is_apply_border,
+        is_apply_number_format,
+        is_apply_protection,
+        is_wrap_text,
+        horizontal_alignment,
+        vertical_alignment
+    )
+VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 
 -- query : select_share_string_table# 
-SELECT format_code, format_id
-FROM number_formats
-ORDER BY id;
+SELECT
+    format_code,
+    format_id
+FROM
+    number_formats
+ORDER BY
+    id;
 
 -- query : select_share_string_table# 
-SELECT font_name,
-       color_theme,
-       color_rgb,
-       family_id,
-       font_size,
-       font_scheme,
-       is_bold,
-       is_italic,
-       is_underline,
-       is_double_underline
-FROM font_style
-ORDER BY id;
+SELECT
+    font_name,
+    color_theme,
+    color_rgb,
+    family_id,
+    font_size,
+    font_scheme,
+    is_bold,
+    is_italic,
+    is_underline,
+    is_double_underline
+FROM
+    font_style
+ORDER BY
+    id;
 
 -- query : select_share_string_table# 
-SELECT background_color_setting,
-       foreground_color_setting,
-       pattern_type
-FROM fill_style
-ORDER BY id;
+SELECT
+    background_color_setting,
+    foreground_color_setting,
+    pattern_type
+FROM
+    fill_style
+ORDER BY
+    id;
 
 -- query : select_share_string_table# 
-SELECT left_border,
-       top_border,
-       right_border,
-       bottom_border
-FROM border_style
-ORDER BY id;
+SELECT
+    left_border,
+    top_border,
+    right_border,
+    bottom_border,
+    diagonal_border
+FROM
+    border_style
+ORDER BY
+    id;
 
 -- query : select_cell_style_table# 
-SELECT format_id,
-       number_format_id,
-       font_id,
-       fill_id,
-       border_id,
-       is_apply_font,
-       is_apply_alignment,
-       is_apply_fill,
-       is_apply_border,
-       is_apply_number_format,
-       is_apply_protection,
-       is_wrap_text,
-       horizontal_alignment,
-       vertical_alignment
-FROM cell_xfs
-ORDER BY id;
+SELECT
+    format_id,
+    number_format_id,
+    font_id,
+    fill_id,
+    border_id,
+    is_apply_font,
+    is_apply_alignment,
+    is_apply_fill,
+    is_apply_border,
+    is_apply_number_format,
+    is_apply_protection,
+    is_wrap_text,
+    horizontal_alignment,
+    vertical_alignment
+FROM
+    cell_xfs
+ORDER BY
+    id;
