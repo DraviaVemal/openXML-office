f2534717b0eb19f287f92c59807423d21fb03a39
Dravia Vemal <contact@draviavemal.com>
2024-12-07 09:08:56 +0000

    formatting update

--- rs/src/spreadsheet/spreadsheet_2007/services/style.rs
+++ rs/src/spreadsheet/spreadsheet_2007/services/style.rs
@@ -1,3 +1,4 @@
+use crate::files::XmlElement;
 use crate::global_2007::traits::XmlDocumentPartCommon;
 use crate::{
     files::{OfficeDocument, XmlDocument},
@@ -5,6 +6,7 @@ use crate::{
     global_2007::traits::XmlDocumentPart,
 };
 use anyhow::{anyhow, Context, Error as AnyError, Result as AnyResult};
+use rusqlite::params;
 use std::{cell::RefCell, collections::HashMap, rc::Weak};
 
 #[derive(Debug)]
@@ -65,18 +67,15 @@ impl XmlDocumentPart for Style {
 }
 
 impl Style {
-    fn load_content_to_database(
+    fn initialize_database(
         office_document: &Weak<RefCell<OfficeDocument>>,
-        xml_document: &mut Weak<RefCell<XmlDocument>>,
+        queries: &HashMap<String, String>,
     ) -> AnyResult<(), AnyError> {
         if let Some(office_doc_ref) = office_document.upgrade() {
             // Decode XML to load in DB
             let office_doc = office_doc_ref
                 .try_borrow()
                 .context("Pulling Office Doc Failed")?;
-            // Load Required Queries
-            let queries = get_all_queries!("style.sql");
-            // Create Required Tables Queries
             let create_query_num_format = queries
                 .get("create_number_format_table")
                 .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
@@ -112,61 +111,182 @@ impl Style {
                 .get_connection()
                 .create_table(&create_query_cell_style)
                 .context("Create Cell Style Table Failed")?;
-            // Insert Tables Queries
-            let insert_query_num_format = queries
-                .get("insert_number_format_table")
-                .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
-            let insert_query_font_style = queries
-                .get("insert_number_format_table")
-                .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
-            let insert_query_fill_style = queries
-                .get("insert_number_format_table")
-                .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
-            let insert_query_border_style = queries
-                .get("insert_number_format_table")
-                .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
-            let insert_query_cell_style = queries
-                .get("insert_number_format_table")
-                .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
+        }
+        Ok(())
+    }
+
+    fn load_content_to_database(
+        office_document: &Weak<RefCell<OfficeDocument>>,
+        xml_document: &mut Weak<RefCell<XmlDocument>>,
+    ) -> AnyResult<(), AnyError> {
+        if let Some(office_doc_ref) = office_document.upgrade() {
+            fn extract_attribute_value(
+                attribute_keys: Vec<&str>,
+                row: &mut Vec<String>,
+                element: &mut Vec<XmlElement>,
+            ) {
+                if let Some(scheme) = element.pop() {
+                    if let Some(attributes) = scheme.get_attribute() {
+                        for attribute_key in attribute_keys {
+                            row.push(
+                                attributes
+                                    .get(attribute_key)
+                                    .unwrap_or(&"".to_string())
+                                    .to_string(),
+                            );
+                        }
+                    }
+                }
+            }
+
+            // Decode XML to load in DB
+            let office_doc = office_doc_ref
+                .try_borrow()
+                .context("Pulling Office Doc Failed")?;
+            // Load Required Queries
+            let queries = get_all_queries!("style.sql");
+            Self::initialize_database(office_document, &queries)
+                .context("Database Initialization Failed")?;
 
             if let Some(xml_doc) = xml_document.upgrade() {
                 let mut xml_doc_mut = xml_doc.try_borrow_mut().context("xml doc borrow failed")?;
-                let number_formats = xml_doc_mut
+                // Load Number Format Region
+                if let Some(number_formats) = xml_doc_mut
                     .pop_elements_by_tag_mut("numFmts", None)
-                    .context("Failed find the Target node")?;
-                if let Some(number_formats_id) = number_formats {}
-                let fonts = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "fonts"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(fonts_id) = fonts {}
-                let fills = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "fills"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(fills_id) = fills {}
-                let borders = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "borders"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(borders_id) = borders {}
-                let cell_xfs = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "cell_xfs"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(cell_xfs_id) = cell_xfs {}
-                let cell_style_xfs = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "cell_style_xfs"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(cell_style_xfs_id) = cell_style_xfs {}
-                let cell_styles = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "cell_styles"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(cell_styles_id) = cell_styles {}
-                let dxfs = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "dxfs"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(dxfs_id) = dxfs {}
-                let table_styles = xml_doc_mut
-                    .get_first_element_id(vec!["styleSheet", "table_styles"], None)
-                    .context("Failed find the Target node")?;
-                if let Some(table_styles_id) = table_styles {}
+                    .context("Failed find the Target node")?
+                    .pop()
+                {
+                    loop {
+                        if let Some(element_id) = number_formats.pop_child_id_mut() {
+                            let num_fmt = xml_doc_mut
+                                .pop_element_mut(&element_id)
+                                .ok_or(anyhow!("Element not Found Error"))?;
+                            if let Some(attributes) = num_fmt.get_attribute() {
+                                let insert_query_num_format = queries
+                                    .get("insert_number_format_table")
+                                    .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
+                                office_doc
+                                    .get_connection()
+                                    .insert_record(
+                                        &insert_query_num_format,
+                                        params![
+                                            attributes
+                                                .get("numFmtId")
+                                                .ok_or(anyhow!("numFmtId Attribute Not Found!"))?,
+                                            attributes.get("formatCode").ok_or(anyhow!(
+                                                "formatCode Attribute Not Found!"
+                                            ))?
+                                        ],
+                                    )
+                                    .context("Number Format Data Insert Failed")?;
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                // Load Font and its child into single row
+                let mut font_row: Vec<String> = Vec::new();
+                if let Some(fonts) = xml_doc_mut
+                    .pop_elements_by_tag_mut("fonts", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {
+                    // fonts
+                    loop {
+                        if let Some(font_id) = fonts.pop_child_id_mut() {
+                            // font
+                            let font = xml_doc_mut
+                                .pop_element_mut(&font_id)
+                                .ok_or(anyhow!("Element not Found Error"))?;
+                            if let Some(mut sz) =
+                                xml_doc_mut.pop_elements_by_tag_mut("sz", Some(&font.get_id()))
+                            {
+                                extract_attribute_value(vec!["val"], &mut font_row, &mut sz);
+                            }
+                            if let Some(mut color) =
+                                xml_doc_mut.pop_elements_by_tag_mut("color", Some(&font.get_id()))
+                            {
+                                extract_attribute_value(
+                                    vec!["rgb", "theme"],
+                                    &mut font_row,
+                                    &mut color,
+                                );
+                            }
+                            if let Some(mut name) =
+                                xml_doc_mut.pop_elements_by_tag_mut("name", Some(&font.get_id()))
+                            {
+                                extract_attribute_value(vec!["val"], &mut font_row, &mut name);
+                            }
+                            if let Some(mut family) =
+                                xml_doc_mut.pop_elements_by_tag_mut("family", Some(&font.get_id()))
+                            {
+                                extract_attribute_value(vec!["val"], &mut font_row, &mut family);
+                            }
+                            if let Some(mut scheme) =
+                                xml_doc_mut.pop_elements_by_tag_mut("scheme", Some(&font.get_id()))
+                            {
+                                extract_attribute_value(vec!["val"], &mut font_row, &mut scheme);
+                            }
+                            // Insert Data into Database
+                            let insert_query_font_style = queries
+                                .get("insert_number_format_table")
+                                .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
+                            office_doc
+                                .get_connection()
+                                .insert_record(&insert_query_font_style, params![])
+                                .context("Insert Font Style Failed")?;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+
+                // Insert Tables Queries
+                let insert_query_fill_style = queries
+                    .get("insert_number_format_table")
+                    .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
+                let insert_query_border_style = queries
+                    .get("insert_number_format_table")
+                    .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
+                let insert_query_cell_style = queries
+                    .get("insert_number_format_table")
+                    .ok_or_else(|| anyhow!("Expected Query Not Found"))?;
+                if let Some(fill) = xml_doc_mut
+                    .pop_elements_by_tag_mut("fills", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
+                if let Some(border) = xml_doc_mut
+                    .pop_elements_by_tag_mut("borders", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
+                if let Some(cell_xf) = xml_doc_mut
+                    .pop_elements_by_tag_mut("cell_xfs", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
+                if let Some(cell_style_xf) = xml_doc_mut
+                    .pop_elements_by_tag_mut("cell_style_xfs", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
+                if let Some(cell_style) = xml_doc_mut
+                    .pop_elements_by_tag_mut("cell_styles", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
+                if let Some(dxf) = xml_doc_mut
+                    .pop_elements_by_tag_mut("dxfs", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
+                if let Some(table_style) = xml_doc_mut
+                    .pop_elements_by_tag_mut("table_styles", None)
+                    .context("Failed find the Target node")?
+                    .pop()
+                {}
             }
         }
         Ok(())
