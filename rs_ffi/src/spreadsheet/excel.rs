d034f671aa27556921bd9d10b9f9611f3a429931
Dravia Vemal <contact@draviavemal.com>
2024-12-18 09:25:30 +0000

    FFI Mapping Update

--- rs_ffi/src/spreadsheet/excel.rs
+++ rs_ffi/src/spreadsheet/excel.rs
@@ -63,6 +63,152 @@ pub extern "C" fn excel_create(
 }
 
 #[no_mangle]
+/// Add New Sheet to the Excel
+pub extern "C" fn excel_add_sheet(
+    excel_ptr: *const c_void,
+    sheet_name: *const c_char,
+    out_worksheet: *mut *mut c_void,
+    out_error: *mut *const c_char,
+) -> i8 {
+    if excel_ptr.is_null() {
+        eprintln!("Received null pointer");
+        return StatusCode::InvalidArgument as i8;
+    }
+    let excel_ptr = excel_ptr as *mut Excel;
+    let mut excel = unsafe { Box::from_raw(excel_ptr) };
+    if sheet_name.is_null() {
+        match excel.add_sheet(None) {
+            Result::Ok(worksheet) => {
+                unsafe {
+                    *out_worksheet = Box::into_raw(Box::new(worksheet)) as *mut c_void;
+                }
+                StatusCode::Success as i8
+            }
+            Err(e) => match CString::new(format!("Flat Buffer Parse Error. {}", e)) {
+                Result::Ok(str) => {
+                    unsafe { *out_error = str.into_raw() };
+                    StatusCode::Success as i8
+                }
+                Err(e) => {
+                    eprintln!("Error String send Error. {}", e);
+                    StatusCode::IoError as i8
+                }
+            },
+        }
+    } else {
+        let sheet_name = unsafe { CStr::from_ptr(sheet_name) }
+            .to_string_lossy()
+            .into_owned();
+        match excel.add_sheet(Some(sheet_name)) {
+            Result::Ok(worksheet) => {
+                unsafe {
+                    *out_worksheet = Box::into_raw(Box::new(worksheet)) as *mut c_void;
+                }
+                StatusCode::Success as i8
+            }
+            Err(e) => match CString::new(format!("Flat Buffer Parse Error. {}", e)) {
+                Result::Ok(str) => {
+                    unsafe { *out_error = str.into_raw() };
+                    StatusCode::Success as i8
+                }
+                Err(e) => {
+                    eprintln!("Error String send Error. {}", e);
+                    StatusCode::IoError as i8
+                }
+            },
+        }
+    }
+}
+
+#[no_mangle]
+/// Get Existing Sheet from Excel
+pub extern "C" fn excel_rename_sheet(
+    excel_ptr: *const c_void,
+    old_sheet_name: *const c_char,
+    new_sheet_name: *const c_char,
+    out_error: *mut *const c_char,
+) -> i8 {
+    if excel_ptr.is_null() || old_sheet_name.is_null() || new_sheet_name.is_null() {
+        eprintln!("Received null pointer");
+        return StatusCode::InvalidArgument as i8;
+    }
+    let excel_ptr = excel_ptr as *mut Excel;
+    let mut excel = unsafe { Box::from_raw(excel_ptr) };
+    let old_sheet_name = unsafe { CStr::from_ptr(old_sheet_name) }
+        .to_string_lossy()
+        .into_owned();
+    let new_sheet_name = unsafe { CStr::from_ptr(new_sheet_name) }
+        .to_string_lossy()
+        .into_owned();
+    match excel.rename_sheet_name(&old_sheet_name, &new_sheet_name) {
+        Result::Ok(()) => StatusCode::Success as i8,
+        Err(e) => match CString::new(format!("Flat Buffer Parse Error. {}", e)) {
+            Result::Ok(str) => {
+                unsafe { *out_error = str.into_raw() };
+                StatusCode::Success as i8
+            }
+            Err(e) => {
+                eprintln!("Error String send Error. {}", e);
+                StatusCode::IoError as i8
+            }
+        },
+    }
+}
+
+#[no_mangle]
+/// Get Existing Sheet from Excel
+pub extern "C" fn excel_get_sheet(
+    excel_ptr: *const c_void,
+    sheet_name: *const c_char,
+    out_worksheet: *mut *mut c_void,
+    out_error: *mut *const c_char,
+) -> i8 {
+    if excel_ptr.is_null() || sheet_name.is_null() {
+        eprintln!("Received null pointer");
+        return StatusCode::InvalidArgument as i8;
+    }
+    let excel_ptr = excel_ptr as *mut Excel;
+    let mut excel = unsafe { Box::from_raw(excel_ptr) };
+    let sheet_name = unsafe { CStr::from_ptr(sheet_name) }
+        .to_string_lossy()
+        .into_owned();
+    match excel.get_worksheet(&sheet_name) {
+        Result::Ok(worksheet) => {
+            unsafe {
+                *out_worksheet = Box::into_raw(Box::new(worksheet)) as *mut c_void;
+            }
+            StatusCode::Success as i8
+        }
+        Err(e) => match CString::new(format!("Flat Buffer Parse Error. {}", e)) {
+            Result::Ok(str) => {
+                unsafe { *out_error = str.into_raw() };
+                StatusCode::Success as i8
+            }
+            Err(e) => {
+                eprintln!("Error String send Error. {}", e);
+                StatusCode::IoError as i8
+            }
+        },
+    }
+}
+
+#[no_mangle]
+/// List Sheet Name from Excel
+pub extern "C" fn excel_list_sheet_name(
+    excel_ptr: *const c_void,
+    out_error: *mut *const c_char,
+) -> i8 {
+    if excel_ptr.is_null() {
+        eprintln!("Received null pointer");
+        return StatusCode::InvalidArgument as i8;
+    }
+    let excel_ptr = excel_ptr as *mut Excel;
+    let mut excel = unsafe { Box::from_raw(excel_ptr) };
+    let sheet_names = excel.list_sheet_names();
+    return StatusCode::InvalidArgument as i8;
+}
+
+#[no_mangle]
 ///Save the Excel File in provided file path
 pub extern "C" fn excel_save_as(
     excel_ptr: *const c_void,
